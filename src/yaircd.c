65a98c6545a8d2b3da39b0bf8cfb4cc18ebfec1b
techupper <techupper@techupper.org>
2013-11-09 18:52:30 +0000

    replaced bzero legacy function with memset

--- src/yaircd.c
+++ src/yaircd.c
@@ -64,7 +64,7 @@ int ircd_boot(void) {
 		return 1;
 	}
 	
-	bzero((char *) &serv_addr, sizeof(serv_addr));
+	memset(&serv_addr, '0', sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	
