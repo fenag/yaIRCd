a6561fdd3c724d441fe205eff6f5cabe6f739a99
techupper <techupper@techupper.org>
2013-11-16 18:00:58 +0000

    in the previous commit I forgot to add a header file, containing structures that are going to be used to store server info

---
+++ src/include/yaIRCd.h
@@ -0,0 +1,30 @@
+#ifndef __IRC_YAIRCD_GUARD__
+#define __IRC_YAIRCD_GUARD__
+
+struct server_info {
+	int id;
+	const char * name;
+	const char * description;
+	const char * net_name;
+	int socket_max_hangup_clients;
+	struct admin_info * admin;
+	struct socket_info * socket_standard;
+	struct socket_info * socket_secure;
+	const char * certificate_path;
+	const char * private_key_path;
+};
+
+struct socket_info {
+	const char * ip;
+	int port;
+	unsigned ssl : 1;
+	int max_hangup_clients;
+};
+
+struct admin_info {
+	const char * name;
+	const char * nick;
+	const char * email;
+};
+
+#endif /* __IRC_YAIRCD_GUARD__ */
